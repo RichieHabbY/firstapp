e1544c30252373fb9224cba2f0fb43fec958df96
tsuibin <tsuibin@qq.com>
2018-03-16 19:47:45 +0800

    我修改了hello.c，添加了一个加法函数

--- hello.c
+++ hello.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+int add(int a, int b)
+{
+	return a + b;
+}
 
 int main(int argc, char * argv[])
 {
 	printf("this is my app codes whit github!\n");
+	int ret = add (3 , 16);
 
+	printf("add ret : %d \n", ret);
 	return 0;
 }
